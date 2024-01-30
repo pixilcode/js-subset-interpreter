dd8aa6c61d12f4095cd7a06329750bc9662356e0
Brendon Bown <brendon.bown@gmail.com>
2024-01-30 01:04:05 -0700

    Finish building AST

--- lib/ast.ml
+++ lib/ast.ml
@@ -1,9 +1,39 @@
 module Literal = struct
-  type t = Number of float | Boolean of bool
+  type t =
+    | Number of float
+    | Boolean of bool
+end
+
+module Binary_operator = struct
+  type t =
+    | Plus
+    | Minus
+    | Times
+    | Divide
+    | Equal
+    | Less_than
+end
+
+module Unary_operator = struct
+  type t =
+    | Negate_bool
+    | Negate_number
+    | Positive
+end
+
+module Logical_operator = struct
+  type t =
+    | And
+    | Or
 end
 
 module Expression = struct
-  type t = Literal of Literal.t
+  type t =
+    | Literal of Literal.t
+    | Binary of t * Binary_operator.t * t
+    | Unary of Unary_operator.t * t
+    | Logical of t * Logical_operator.t * t
+    | Conditional of t * t * t
 end
 
 module Statement = struct
