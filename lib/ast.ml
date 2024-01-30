04fbd1547130b38630036bc0f1a9e078012c5c48
Brendon Bown <brendon.bown@gmail.com>
2024-01-30 00:55:46 -0700

    Start AST

--- lib/ast.ml
+++ lib/ast.ml
@@ -1,1 +1,17 @@
+module Literal = struct
+  type t = Number of float | Boolean of bool
+end
+
+module Expression = struct
+  type t = Literal of Literal.t
+end
+
+module Statement = struct
+  type t = Expression_statement of Expression.t
+end
+
+module Program = struct
+  type t = Program of Statement.t
+end
+
 let from_json _json = failwith "Not implemented"
