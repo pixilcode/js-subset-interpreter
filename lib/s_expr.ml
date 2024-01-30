55e48905cf1425ec80c74affe3e70b2600000d16
Brendon Bown <brendon.bown@gmail.com>
2024-01-30 00:55:37 -0700

    Start working on sexpr module

--- lib/s_expr.ml
+++ lib/s_expr.ml
@@ -1,3 +1,10 @@
+type t =
+  | Expr of t list
+  | Atom of string
+
 let from_ast _ast = failwith "Not implemented"
 
-let pretty_print _s_expr = failwith "Not implemented"
+let rec pretty_print s_expr =
+  match s_expr with
+  | Expr l -> "(" ^ String.concat " " (List.map pretty_print l) ^ ")"
+  | Atom s -> s
