4b1b37e8bbbcf07ae6e30fc4000022523e60b230
Brendon Bown <brendon.bown@gmail.com>
2024-02-01 16:26:58 -0700

    Finish basic tests

--- test/test_js_interpret.ml
+++ test/test_js_interpret.ml
@@ -6,7 +6,8 @@ let run_test (name, js) =
 
   let json = Util.run_acorn js in
   let ast = Ast.from_json_string json in
-  let s_expr = S_expr.from_ast ast in
+  let result = Interpreter.interpret ast in
+  let s_expr = S_expr.from_result result in
 
   print_endline (S_expr.to_string s_expr);
   print_newline ()
@@ -14,9 +15,10 @@ let run_test (name, js) =
 let tests = [
   ("simple_number", "1");
   ("unary", "!true");
-  ("arithmetic", "1 + 2 - 3 * 4 / 5");
+  ("arithmetic", "1 + 2 - 3 * 4 / 6");
   ("relational", "1 < 2 == true");
   ("logical", "true && false || false")
+  (* TODO: write tests that produce errors *)
 ]
 
 (* iterate over all the tests and run them *)
--- test/test_js_print_ast.ml
+++ test/test_js_print_ast.ml
@@ -1,32 +1,10 @@
 open Js_interpreter
 
-let run_acorn js =
-  (* write the js to a temp file *)
-  let temp_js_file = Filename.temp_file "acorn" ".js" in
-  let temp_json_file = Filename.temp_file "acorn" ".json" in
-  let oc = open_out temp_js_file in
-  output_string oc js;
-  close_out oc;
-
-  (* run acorn on the temp file *)
-  ignore (Sys.command ("acorn --ecma2024 " ^ temp_js_file ^ " > " ^ temp_json_file) : int);
-
-  (* read the result *)
-  let ic = open_in_bin temp_json_file in
-  let json = really_input_string ic (in_channel_length ic) in
-  close_in ic;
-
-  (* delete the temp files *)
-  Sys.remove temp_js_file;
-  Sys.remove temp_json_file;
-
-  json
-
 (* run a test and print the result *)
 let run_test (name, js) =
   print_endline ("--- " ^ name ^ " ---");
 
-  let json = run_acorn js in
+  let json = Util.run_acorn js in
   let ast = Ast.from_json_string json in
   let s_expr = S_expr.from_ast ast in
 
