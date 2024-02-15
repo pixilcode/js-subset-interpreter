df135270704c7f9df0fe8743889d68a48d2e5355
Brendon Bown <brendon.bown@gmail.com>
2024-02-15 00:50:23 -0700

    Update interpreter with new statements

--- lib/interpreter.ml
+++ lib/interpreter.ml
@@ -3,88 +3,128 @@ open Core
 let error msg =
   Error ("Oh bananas! " ^ msg)
 
+module Env: sig
+  type ident = string
+  type t
+
+  val empty: t
+  val get: t -> ident -> Value.t option
+  val set: t -> ident -> Value.t -> t
+end = struct
+  type ident = string
+  type t = (ident, Value.t) Hashtbl.t
+
+  let empty = Hashtbl.create (module String)
+
+  let get env ident = Hashtbl.find env ident
+
+  let set env ident value =
+    Hashtbl.set ~key:ident ~data:value env;
+    env
+end
+
 let interpret ast =
   let open Ast in
   let open Core.Result.Monad_infix in
-  let eval_literal l =
+  let eval_literal l: Value.t =
     match l with
-    | Literal.Number i -> Ok (Value.Number i)
-    | Literal.Boolean b -> Ok (Value.Boolean b)
+    | Literal.Number i -> Value.Number i
+    | Literal.Boolean b -> Value.Boolean b
   in
 
-  let rec eval_expression e =
+  let rec eval_expression e env: (Value.t * Env.t, string) result =
     match e with
-    | Expression.Literal l -> eval_literal l
+    | Expression.Literal l -> Ok (eval_literal l, env)
     | Expression.Binary (lhs, op, rhs) -> begin
-      eval_expression lhs >>= fun lhs ->
-      eval_expression rhs >>= fun rhs ->
+      eval_expression lhs env >>= fun (lhs, env) ->
+      eval_expression rhs env >>= fun (rhs, env) ->
       match (lhs, rhs) with
       | (Value.Number lhs, Value.Number rhs) -> begin
         let open Binary_operator in
         match op with
-        | Plus -> Ok (Value.Number (lhs + rhs))
-        | Minus -> Ok (Value.Number (lhs - rhs))
-        | Times -> Ok (Value.Number (lhs * rhs))
+        | Plus -> Ok (Value.Number (lhs + rhs), env)
+        | Minus -> Ok (Value.Number (lhs - rhs), env)
+        | Times -> Ok (Value.Number (lhs * rhs), env)
         | Divide ->
           if rhs = 0 then
             error "Cannot divide by zero"
           else
-            Ok (Value.Number (lhs / rhs))
-        | Equal -> Ok (Value.Boolean (lhs = rhs))
-        | Less_than -> Ok (Value.Boolean (lhs < rhs))
+            Ok (Value.Number (lhs / rhs), env)
+        | Equal -> Ok (Value.Boolean (lhs = rhs), env)
+        | Less_than -> Ok (Value.Boolean (lhs < rhs), env)
       end
       | _ -> error "Invalid arithmetic operation" (* TODO: better error message *)
     end
     | Expression.Logical (lhs, op, rhs) -> begin
-      eval_expression lhs >>= fun lhs ->
+      eval_expression lhs env >>= fun (lhs, env) ->
       let open Logical_operator in
       match lhs, op with
       (* short-circuit boolean operators *)
       | Value.Boolean false, And
-      | Value.Boolean true, Or -> Ok lhs
+      | Value.Boolean true, Or -> Ok (lhs, env)
       (* evaluate second argument*)
       | Value.Boolean true, And
       | Value.Boolean false, Or -> begin
-        eval_expression rhs >>= fun rhs ->
+        eval_expression rhs env >>= fun (rhs, env) ->
         match rhs with 
-        | Value.Boolean _ -> Ok rhs
+        | Value.Boolean _ -> Ok (rhs, env)
         | _ -> error "Invalid logical operation" (* TODO: better error message *)
       end
       (* invalid operators *)
       | _ -> error "Invalid logical operation" (* TODO: better error message *)
     end
     | Expression.Unary (op, rhs) -> begin
-      eval_expression rhs >>= fun rhs ->
+      eval_expression rhs env >>= fun (rhs, env) ->
       let open Unary_operator in
       match (op, rhs) with
-      | (Positive, Value.Number rhs) -> Ok (Value.Number (-rhs))
-      | (Negate_number, Value.Number rhs) -> Ok (Value.Number (rhs))
-      | (Negate_bool, Value.Boolean rhs) -> Ok (Value.Boolean (not rhs))
+      | (Positive, Value.Number rhs) -> Ok (Value.Number (-rhs), env)
+      | (Negate_number, Value.Number rhs) -> Ok (Value.Number (rhs), env)
+      | (Negate_bool, Value.Boolean rhs) -> Ok (Value.Boolean (not rhs), env)
       | _ -> error "Invalid unary operation" (* TODO: better error message *)
       end
     | Expression.Conditional (cond, if_t, if_f) -> begin
-      eval_expression cond >>= fun cond ->
+      eval_expression cond env >>= fun (cond, env) ->
       match cond with
       | Value.Boolean cond ->
         if cond then
-          eval_expression if_t
+          eval_expression if_t env
         else
-          eval_expression if_f
+          eval_expression if_f env
       | _ -> error "Invalid condition in conditional"
-
     end
+    | Expression.Identifier ident ->
+      let value = Env.get env ident in
+      let ident_error = error "Undefined identifier" in
+      Option.value_map value ~default:ident_error ~f:(
+        fun value -> Ok (value, env)
+      )
   in
 
-  let eval_statement s =
+  let eval_statement s env: (Value.t option * Env.t, string) result =
     match s with
     | Statement.Expression_statement e ->
-      eval_expression e
+      eval_expression e env >>| fun (value, env) ->
+        (Some value, env)
+    | Statement.Variable_declaration decls ->
+      let env = List.fold decls ~init:(Ok env) ~f:(fun env (ident, expr) ->
+        env >>= fun env ->
+        eval_expression expr env >>= fun (value, env) ->
+        let env = Env.set env ident value in
+        Ok (env)
+      ) in
+      env >>| fun env ->
+      (None, env)
   in
   
-  let eval_program p =
+  let eval_program p env: (Value.t list * Env.t, string) result =
     match p with
-    | Program.Program s ->
-      eval_statement s
+    | Program.Program stmts ->
+      List.fold stmts ~init:(Ok ([], env)) ~f:(fun prev_result s ->
+        prev_result >>= fun (prev_outputs, env) -> 
+        eval_statement s env >>= fun (output, env) ->
+        let output = Option.to_list output in
+        Ok (prev_outputs @ output, env)
+      )
   in
   
-  eval_program ast
+  eval_program ast Env.empty
