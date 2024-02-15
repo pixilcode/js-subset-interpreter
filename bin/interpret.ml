85a573c3b7e3c796ab4e76846c921aab9887ac86
Brendon Bown <brendon.bown@gmail.com>
2024-02-15 01:03:41 -0700

    Fix interpreter binary

--- bin/interpret.ml
+++ bin/interpret.ml
@@ -4,5 +4,14 @@ let () =
   let input = Util.read_stdin () in
   let ast = Parser.from_json_string input in
   let result = Interpreter.interpret ast in
-  let s_expr = S_expr.from_result result in
-  print_endline (S_expr.to_string s_expr)
+  match result with
+  | Ok (values, _env) ->
+    let values = List.map S_expr.from_value values in
+    let values = List.map S_expr.to_string values in
+    List.iter print_endline values;
+    print_newline ()
+  | Error message ->
+    let message = S_expr.from_error message in
+    let message = S_expr.to_string message in
+    print_endline message;
+    print_newline ()
